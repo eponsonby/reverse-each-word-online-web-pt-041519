ad9bb31108864946827dd5296f1d67d0a65cb4f2
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 18:10:13 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence_string)
+  array = sentence_string.split
+    array.collect do |word|
+      word.reverse
+  end
+  puts array
+end
+
+string = "Hello how are you?"
+reverse_each_word(string)
